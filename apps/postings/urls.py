403e7f55ee9453799df421072897f07971923a31
한송희 <98_han@naver.com>
2022-09-26 14:29:03 +0900

    Feat : viewset을 이용한 게시글 CRUD 개발(게시글 생성 초점)
    - viewset을 이용하여 간단한 CRUD를 구현하고, 게시글 생성 API를 집중적으로 개발
    - token 정보를 바탕으로 user_id는 자동으로 추가됨

--- apps/postings/urls.py
+++ apps/postings/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from apps.postings.views import PostingViewSet
+
+router = routers.DefaultRouter()
+router.register('', PostingViewSet, basename='postings')
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
--- apps/postings/views.py
+++ apps/postings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.postings.models import Posting
+from apps.postings.serializers import PostingListSerializer, PostingCreateSerializer, PostingDetailSerializer
+
+
+class PostingViewSet(viewsets.ModelViewSet):
+    queryset = Posting.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostingListSerializer
+        if self.action == "create":
+            return PostingCreateSerializer
+        if self.action == "retrieve":
+            return PostingDetailSerializer
+        return PostingListSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
