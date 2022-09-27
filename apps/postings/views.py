19d8bd9822de2f3fb785a9b2bdc962ef7fb20e65
한송희 <98_han@naver.com>
2022-09-27 21:59:00 +0900

    Feat : 게시글 목록 정렬, 검색, 필터링, 페이지 기능 구현
    - ordering으로 원하는 필드와 오름차/내림차로 정렬 가능(default:최근 작성일)
    - search로 제목에 해당 키워드를 포함한 게시물 조회 가능
    - 지정한 hashtag를 포함한 게시물 조회 가능
    - page_size로 한 페이지 당 게시글 수 조정 가능

--- apps/postings/views.py
+++ apps/postings/views.py
@@ -1,12 +1,46 @@
+from django.db.models import Q
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+from rest_framework.pagination import PageNumberPagination
 
 from apps.postings.models import Posting
 from apps.postings.serializers import PostingListSerializer, PostingCreateSerializer, PostingDetailSerializer
 
 
+class PostingSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class PostingViewSet(viewsets.ModelViewSet):
-    queryset = Posting.objects.all()
+    pagination_class = PostingSetPagination
+
+    def get_queryset(self):
+        queryset = Posting.objects.prefetch_related('hashtag').all()
+        search_condition = self.request.GET.get('search', None)
+        hashtag_condition = self.request.GET.get('hashtags', None)
+        ordering = self.request.GET.get('ordering', '-create_time')
+
+        try:
+            q = Q()
+
+            if search_condition:
+                q.add(Q(title__contains=search_condition), q.AND)
+
+            if hashtag_condition:
+                q_hashtag = Q()
+                for tag in hashtag_condition.split(','):
+                    q_hashtag.add(Q(hashtag__name=tag), q_hashtag.OR)
+                q.add(q_hashtag, q.AND)
+
+            queryset = queryset.filter(q).distinct().order_by(ordering)
+            return queryset
+
+        except Exception as e:
+            raise APIException(detail=f'에러가 발생하였습니다. {e}')
 
     def get_serializer_class(self):
         if self.action == "list":
