ecb68badc5ff5dcca459b7e9a59331a3d5f23367
한송희 <98_han@naver.com>
2022-10-28 17:55:19 +0900

    Fix : 누락된 사용자 모델 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -198,6 +198,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.User'
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 CSRF_TRUSTED_ORIGINS= ['https://*.azurewebsites.net']
